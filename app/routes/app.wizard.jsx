e1fab85b256bfd3440b66ff70bbf8e5dd38c6578
Valux17 <losiv20@itisvinci.com>
2024-03-08 15:04:43 +0100

    Create new branch

--- app/routes/app.wizard.jsx
+++ app/routes/app.wizard.jsx
@@ -8,9 +8,19 @@ import {
     Text,
     BlockStack,
     IndexTable,
+    Badge,
   } from "@shopify/polaris";
 
-
+  const orders = [
+    {
+      id: '1020',
+      order: '#1020',
+      date: 'Jul 20 at 4:34pm',
+      customer: 'Jaydon Stanton',
+      total: '$969.44',
+      paymentStatus: <Badge progress="complete">Paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },]
 
   export default function Wizard() {
     return (
